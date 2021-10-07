dbb6717176fd8c7171929717b3a71717b225b110
zhk <thurston1982@gmail.com>
2021-10-07 21:13:27 +0800

    polish code

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/cncamp/golang
-
-go 1.16
-
-require (
-	github.com/golang/glog v0.0.0-20210429001901-424d2337a529
-	github.com/stretchr/testify v1.7.0
-)
---
+++ httpserverhomework/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+)
+
+func main() {
+	flag.Set("v", "4")
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/healthz", healthz)
+	c, python, java := true, false, "no!"
+	fmt.Println(c, python, java)
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok\n")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entering root handler")
+	user := r.URL.Query().Get("user")
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+}
